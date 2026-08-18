9283e48216faee4f1c56b6dae3869599fe8ac4e6
sabbirba <sabbirba10@gmail.com>
2026-08-18 16:48:16 +0600

    ci: update sha256 checksums for v0.1.2

--- Formula/preprinttui.rb
+++ Formula/preprinttui.rb
@@ -1,20 +1,20 @@
 class Preprinttui < Formula
   desc "Interactive Terminal User Interface for PreConnect printer"
   homepage "https://preconnect.app"
-  version "0.1.1"
+  version "0.1.2"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sabbirba/preprinttui/releases/download/v#{version}/preprinttui-macos-arm64.tar.gz"
-      sha256 "865ab6aca47088809cc0ed9e089434f0f83a7645751a8ee9be97d974315c026e"
+      sha256 "9f9c7682cb1b6d669edb9c7cebe2e4b5425d7a500ee69d425d663b9ad7466348"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sabbirba/preprinttui/releases/download/v#{version}/preprinttui-linux-musl-x86_64.tar.gz"
-      sha256 "fd5f4dcaf4dd4e874c1b2adc6fbeccd8e312f22e8f5098950b8bbe744ad65f3e"
+      sha256 "e754067d1f63232ab28a375974547f395766efa433a43fa2b72fc86c7b7b3497"
     end
   end
 
